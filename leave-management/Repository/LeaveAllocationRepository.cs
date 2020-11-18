21047017355a8d35cf9f4495113a9c7d6e483674
SaadAbdullahAlqhtani <73571974+SaadAbdullahAlqhtani@users.noreply.github.com>
2020-11-18 04:43:31 +0300

    Add project files.

---
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using leave_management.Contracts;
+using leave_management.Data;
+
+namespace leave_management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            var leaveallocations = _db.LeaveAllocations.ToList();
+            return leaveallocations;
+        }
+
+        public LeaveAllocation FindById(int Id)
+        {
+            var leaveallocation = _db.LeaveAllocations.Find(Id);
+            return leaveallocation;
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Save()
+        {
+           var changes = _db.SaveChanges();
+           return changes > 0;
+        }
+
+        public ICollection<LeaveAllocation> GetEmployeesByLeaveAllocations(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ leave-management/Repository/LeaveHistoryRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using leave_management.Contracts;
+using leave_management.Data;
+
+namespace leave_management.Repository
+{
+    public class LeaveHistoryRepository : ILeaveHistoryRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public LeaveHistoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public ICollection<LeaveHistory> FindAll()
+        {
+            var leavehistories = _db.LeaveHistories.ToList();
+            return leavehistories;
+        }
+
+        public LeaveHistory FindById(int Id)
+        {
+            var leavehistory = _db.LeaveHistories.Find(Id);
+            return leavehistory;
+        }
+
+        public bool Create(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Add(entity);
+            return Save();
+        }
+
+        public bool Update(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Update(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveHistory entity)
+        {
+            _db.LeaveHistories.Remove(entity);
+            return Save();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public ICollection<LeaveHistory> GetEmployeesByLeaveHistories(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
