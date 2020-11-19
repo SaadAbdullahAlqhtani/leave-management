7db964d483fb4dfd9a86365efd13ffe208c4eb01
SaadAbdullahAlqhtani <73571974+SaadAbdullahAlqhtani@users.noreply.github.com>
2020-11-19 09:10:22 +0300

    Setup User Auth, Default roles and admin,modified registration process

--- leave-management/Controllers/LeaveTypesController.cs
+++ leave-management/Controllers/LeaveTypesController.cs
@@ -8,9 +8,12 @@ using AutoMapper;
 using leave_management.Contracts;
 using leave_management.Data;
 using leave_management.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace leave_management.Controllers
 {
+
+    [Authorize(Roles = "Administrator")]
     public class LeaveTypesController : Controller
     {
         private readonly ILeaveTypeRepository _repo;
@@ -26,7 +29,7 @@ namespace leave_management.Controllers
        
 
        
-
+        
         // GET: LeaveTypesController
         public ActionResult Index()
         {
